032b0dc508b278a4c2ec1013414a03b78dad25ed
Seth Peasley <seth.peasley@gmail.com>
2017-07-04 18:09:22 -0400

    Updated comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,7 @@
-/* Blink without Delay
+/* Garden Party Main code
 
-Turns on and off a light emitting diode (LED) connected to a digital
-pin, without using the delay() function.  This means that other code
-can run at the same time without being interrupted by the LED code.
+Initializing code and main loop for Garden Party.
 
-The circuit:
- * Use the onboard LED.
- * Note: Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-
-created 2005
-by David A. Mellis
-modified 8 Feb 2010
-by Paul Stoffregen
-modified 11 Nov 2013
-by Scott Fitzgerald
-modified 9 Jan 2017
-by Arturo Guadalupi
-
-This example code is in the public domain.
-
-http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
 */
 
 #include <Arduino.h>
@@ -32,7 +10,6 @@ http://www.arduino.cc/en/Tutorial/BlinkWithoutDelay
 #include <Adafruit_MAX31856.h>
 
 const int LEDPin = 13; //LED_BUILTIN; // the number of a LED pin
-
 int ledState = LOW; // LED state used to set the LED
 
 // Generally, we want to use an unsigned long for variables that
@@ -66,7 +43,6 @@ void setup()
 
   myThermocouple.begin(); // type K Thermocouple
   myThermocouple.setThermocoupleType(MAX31856_TCTYPE_K);
-
 
   pinMode(LEDPin, OUTPUT);
 }
